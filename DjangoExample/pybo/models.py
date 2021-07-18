ca1fadbc5b21b3380219f2dabd0d1ba16cc8bcf4
SpellBound <junlee36520620@gmail.com>
2021-07-18 20:26:35 +0900

    pyboCofig 설정 오류

--- DjangoExample/pybo/models.py
+++ DjangoExample/pybo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# 질문 게시글
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+
+# 답변 게시글
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
