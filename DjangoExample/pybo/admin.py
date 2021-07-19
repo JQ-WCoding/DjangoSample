e0eb888b8ff0bc133069d451405530b2aa511f2b
SpellBound <junlee36520620@gmail.com>
2021-07-19 22:51:38 +0900

    admin 기능 추가
    html을 이용해 목록 및 상세페이지 구현

--- DjangoExample/pybo/admin.py
+++ DjangoExample/pybo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject', 'content'] # 제목과, 내용으로 검색하기
+
+
+admin.site.register(Question, QuestionAdmin)
