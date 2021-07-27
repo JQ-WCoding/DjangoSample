8ee5ecf2d9b2340ed6a5eaf3f4adc1400c616c4e
SpellBound <junlee36520620@gmail.com>
2021-07-27 22:02:48 +0900

    질문 갯수 출력

--- DjangoExample/config/urls.py
+++ DjangoExample/config/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 # --------------------------------------------------------------------------- #
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
 ]
